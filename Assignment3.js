f4d148e64ae81abd1d7ad06e028a6e238a8069ce
mahak02Shukla <99255298+mahak02Shukla@users.noreply.github.com>
2022-02-09 14:32:37 +0530

    Add files via upload

---
+++ Assignment3.js
@@ -0,0 +1,6 @@
+var sum=0,num;
+while(num > -1){
+    sum = sum + num;
+    num = parseInt(prompt("Enter a number: "))
+}
+console.log("Total should be: "+ sum);
---
+++ OddEven.js
@@ -0,0 +1,8 @@
+var num = parseInt(prompt("Enter a number to check Even or Odd: "));
+
+if(num%2==0){
+    console.log(num + " is EVEN");
+}
+else{
+    console.log(num + "is ODD");
+}
